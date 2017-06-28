0c247767f8f738446013a1d2224bf9fc76df8151
Deny Chen <deny.chen@gmail.com>
2017-06-28 15:52:16 -0700

    Fix finding of story record

--- app/routes/stories/story.js
+++ app/routes/stories/story.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.get('store').find('story', params.storyId);
+    return this.get('store').findRecord('story', params.storyId);
   }
 });
