5b0afb7459fb2204c6b016b5a1c10c3e91ace535
Deny Chen <deny.chen@gmail.com>
2017-06-28 15:52:57 -0700

    Add description to story model

--- app/controllers/stories/story.js
+++ app/controllers/stories/story.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   story: Ember.computed.readOnly('model'),
   title: Ember.computed.readOnly('story.title'),
+  description: Ember.computed.readOnly('story.description'),
   firstPageId: Ember.computed.readOnly('story.firstPageId'),
   genres: Ember.computed.readOnly('story.genres'),
   formattedGenres: Ember.computed('genres', function() {
