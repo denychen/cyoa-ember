57219ffdb3458b418627f21b903c776c60644e00
Deny Chen <deny.chen@gmail.com>
2017-06-28 16:55:57 -0700

    Add Roboto font and update UI

--- app/components/story-card.js
+++ app/components/story-card.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  storyId: Ember.computed.readOnly('story.id'),
+  title: Ember.computed.readOnly('story.title'),
+  description: Ember.computed.readOnly('story.description'),
   titleFirstLetter: Ember.computed('title', function() {
     return this.get('title').charAt(0);
   })
