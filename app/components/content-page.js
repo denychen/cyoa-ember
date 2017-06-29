9500f812d08f40af28c2036e6239a8b0e2aa9c15
Deny Chen <deny.chen@gmail.com>
2017-06-29 16:45:29 -0700

    Default back to title page when navigating away from a content page

--- app/components/content-page.js
+++ app/components/content-page.js
@@ -3,6 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: '',
 
+  willDestroyElement() {
+    this._super(...arguments);
+    this.set('page', null);
+  },
+
   page: null,
   sortedDestinations: Ember.computed('page.destinations', function() {
     return this.get('page.destinations').sortBy('order');
