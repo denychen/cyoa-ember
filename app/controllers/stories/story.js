d22bdc16d916170369732333352e72bc80b2bcdc
Deny Chen <deny.chen@gmail.com>
2017-06-29 18:42:41 -0700

    Update button stylings

--- app/controllers/stories/story.js
+++ app/controllers/stories/story.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   page: null,
   story: Ember.computed.readOnly('model'),
+  hasDestinations: Ember.computed.notEmpty('page.destinations'),
   actions: {
     loadNextPage(id) {
       this.store.find('page', id).then(page => {
