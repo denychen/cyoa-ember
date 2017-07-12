5d577a34c542e0e77e755f808a8ccda44e16d69a
Deny Chen <deny.chen@gmail.com>
2017-07-12 11:25:09 -0700

    Update signup call

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -5,22 +5,24 @@ export default Ember.Controller.extend({
   
   actions: {
     signup() {
-      this.get('store').createRecord('user', {
-        email: this.get('email'),
-        password: this.get('password'),
-        username: this.get('username')
-      }).save().then(user => {
+      Ember.$.ajax({
+        url: 'http://localhost:3000/api/users/signup',
+        type: 'POST',
+        contentType: 'application/json',
+        dataType : 'json',
+        data: JSON.stringify({
+          "email": this.get('email'),
+          "password": this.get('password'),
+          "username": this.get('username')
+        })
+      }).then(() => {
+        let credentials = { email: this.get('email'), password: this.get('password') };
+
         this.set('email', null);
         this.set('password', null);
         this.set('username', null);
 
-        new Ember.RSVP.Promise((resolve, reject) => {
-          return (this.get('session.isAuthenticated')) ? this.get('session').invalidate() : resolve(true);
-        }).then (() => {
-          let credentials = { email: user.get('email'), password: user.get('password') };
-
-          return this.get('session').authenticate('authenticator:custom', credentials);
-        });
+        return this.get('session').authenticate('authenticator:custom', credentials);
       });
     }
   }
