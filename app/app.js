382836530e1c79f4f1ab31bfbacc41d8874cd6bd
Deny Chen <deny.chen@gmail.com>
2017-07-14 13:45:26 -0700

    Update ember version

--- app/app.js
+++ app/app.js
@@ -5,6 +5,8 @@ import config from './config/environment';
 
 let App;
 
+Ember.MODEL_FACTORY_INJECTIONS = true;
+
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
