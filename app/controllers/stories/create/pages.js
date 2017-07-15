8ca0909ae770e9450da27202203901ced76c1595
Deny Chen <deny.chen@gmail.com>
2017-07-14 17:53:55 -0700

    Basic layout of create page route

--- app/controllers/stories/create/pages.js
+++ app/controllers/stories/create/pages.js
@@ -2,5 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   story: Ember.computed.readOnly('model.story'),
-  pages: Ember.computed.readOnly('story.pages')
+  pages: Ember.computed.readOnly('story.pages'),
+
+  activePage: Ember.computed('pages', function() {
+    return this.get('pages.firstObject');
+  }),
+
+  actions: {
+    selectPage(id) {
+      let activePage = this.get('pages').find(page => page.id === id);
+      this.set('activePage', activePage);
+    }
+  }
 });
--- tests/integration/components/page-item-test.js
+++ tests/integration/components/page-item-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('option-editor', 'Integration | Component | option editor', {
+moduleForComponent('page-item', 'Integration | Component | page item', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{option-editor}}`);
+  this.render(hbs`{{page-item}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#option-editor}}
+    {{#page-item}}
       template block text
-    {{/option-editor}}
+    {{/page-item}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
