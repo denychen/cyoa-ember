afe334ab59aa519cd8a6a9733399b3da3dd3a43f
Deny Chen <deny.chen@gmail.com>
2017-07-25 16:24:46 -0700

    Create a new blank page if story has no pages when editing the story

--- app/routes/stories/create/pages.js
+++ app/routes/stories/create/pages.js
@@ -7,5 +7,16 @@ export default Ember.Route.extend({
     return RSVP.hash({
       story: this.get('store').findRecord('story', params.storyId, { reload: true })
     });
+  },
+
+  setupController(controller, model) {
+    let pages = model.story.get('pages');
+
+    if (pages.length === 0) {
+      let newPage = this.get('store').createRecord('page');
+      pages.pushObject(newPage);
+    }
+
+    controller.set('model', model);
   }
 });
