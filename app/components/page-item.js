4a03b36061fa8b3d6148c418529e44248c8e0e04
Deny Chen <deny.chen@gmail.com>
2017-07-25 17:57:49 -0700

    Fix flashing of dirty icon when initially creating a page

--- app/components/page-item.js
+++ app/components/page-item.js
@@ -4,8 +4,12 @@ export default Ember.Component.extend({
   classNameBindings: ['isActivePage:o__active-page'],
   defaultPageName: 'Untitled page',
   pageName: Ember.computed.or('page.name', 'defaultPageName'),
-  isDirty: Ember.computed('page.hasDirtyAttributes', function() {
-    return this.get('page.hasDirtyAttributes');
+  isDirty: Ember.computed('page.id', 'page.hasDirtyAttributes', function() {
+    if (this.get('page.id')) {
+      return this.get('page.hasDirtyAttributes');
+    } else {
+      return false;
+    }
   }),
   isActivePage: Ember.computed('page.id', 'activePage.id', function() {
     return this.get('page.id') === this.get('activePage.id');
