70d442429884e011dc3c7f8fbe823a731b8b545c
Deny Chen <deny.chen@gmail.com>
2017-07-26 18:54:52 -0700

    Redirect to create story route when visiting create page route for a story the user is not an author of

--- app/routes/stories/create/pages.js
+++ app/routes/stories/create/pages.js
@@ -3,10 +3,22 @@ import RSVP from 'rsvp';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  currentUser: Ember.inject.service('current-user'),
+  
   model(params) {
     return RSVP.hash({
       story: this.get('store').findRecord('story', params.storyId, { reload: true })
     });
+  },
+
+  afterModel(model, transition) {    
+    let isAuthor = model.story.get('authors').any(author => {
+      return author.get('id') === this.get('currentUser.user.id');
+    });
+
+    if (!isAuthor) {
+      this.replaceWith('stories.create');
+    }
   },
 
   setupController(controller, model) {
