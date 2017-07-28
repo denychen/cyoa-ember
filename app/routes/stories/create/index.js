5068e8cb0704d49db6272d75cdd4b4703718bd07
Deny Chen <deny.chen@gmail.com>
2017-07-28 15:38:35 -0700

    Clear create story fields when leaving the route

--- app/routes/stories/create/index.js
+++ app/routes/stories/create/index.js
@@ -4,5 +4,13 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model() {
     return this.store.findAll('genre');
+  },
+
+  actions: {
+    willTransition(transition) {
+      this.set('controller.title', null);
+      this.set('controller.premise', null);
+      this.set('controller.selectedGenres', null);
+    }
   }
 });
