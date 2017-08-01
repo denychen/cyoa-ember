48c84a53744d620d29cfeebe4c2999305665a7dc
Deny Chen <deny.chen@gmail.com>
2017-08-01 15:58:38 -0700

    Update first page indicator

--- app/components/page-item.js
+++ app/components/page-item.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNameBindings: ['isActivePage:o__active-page', 'isFirstPage:page-creator__first-page__indicator'],
+  classNameBindings: ['isActivePage:o__active-page', 'isFirstPage:o__list__first-page', 'isNotFirstPage:o__list__not-first-page'],
   defaultPageName: 'Untitled page',
   pageName: Ember.computed.or('page.name', 'defaultPageName'),
   isDirty: Ember.computed('page.id', 'page.hasDirtyAttributes', function() {
@@ -16,5 +16,6 @@ export default Ember.Component.extend({
   }),
   isFirstPage: Ember.computed('page.id', 'page.story.firstPageId', function() {
     return this.get('page.id') === this.get('page.story.firstPageId');
-  })
+  }),
+  isNotFirstPage: Ember.computed.not('isFirstPage')
 });
