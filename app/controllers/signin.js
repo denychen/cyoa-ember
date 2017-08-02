5e8e9249f994d69ec4a9236e7e94f96fd9f6c808
Deny Chen <deny.chen@gmail.com>
2017-08-01 18:21:57 -0700

    Field checks for sign in page

--- app/controllers/signin.js
+++ app/controllers/signin.js
@@ -2,16 +2,35 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
+  hasErrors: Ember.computed.or('missingEmail', 'missingPassword'),
+  hasNoErrors: Ember.computed.not('hasErrors'),
 
   actions: {
     signin() {
-      new Ember.RSVP.Promise((resolve/*, reject*/) => {
-        return (this.get('session.isAuthenticated')) ? this.get('session').invalidate() : resolve(true);
-      }).then (() => {
-        let credentials = { email: this.get('email'), password: this.get('password') };
+      let email = this.get('email');
+      let password = this.get('password');
 
-        return this.get('session').authenticate('authenticator:custom', credentials);
-      });
+      if (email) {
+        this.set('missingEmail', false);
+      } else {
+        this.set('missingEmail', true);
+      }
+
+      if (password) {
+        this.set('missingPassword', false);
+      } else {
+        this.set('missingPassword', true);
+      }
+
+      if (this.get('hasNoErrors')) {
+        new Ember.RSVP.Promise((resolve/*, reject*/) => {
+          return (this.get('session.isAuthenticated')) ? this.get('session').invalidate() : resolve(true);
+        }).then (() => {
+          let credentials = { email: email, password: password };
+
+          return this.get('session').authenticate('authenticator:custom', credentials);
+        });
+      }
     }
   }
 });
