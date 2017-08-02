a4017ea6f89b822c17ae9555687fd9f254b44995
Deny Chen <deny.chen@gmail.com>
2017-08-01 18:04:05 -0700

    Add warning for missing old password when changing settings
    Do not call endpoint if no settings are changed

--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -3,38 +3,48 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   currentUser: Ember.inject.service('current-user'),
   session: Ember.inject.service('session'),
+  changedSetting: Ember.computed.or('email', 'username', 'password'),
 
   actions: {
     saveSettings() {
       let user = this.get('currentUser.user');
+      let oldPassword = this.get('oldPassword');
 
-      new Ember.RSVP.Promise((/*resolve, reject*/) => {
-        Ember.$.ajax({
-          url: 'http://localhost:3000/api/users/',
-          type: 'PUT',
-          headers: { 'Authorization': `Bearer ${user.get('token')}` },
-          contentType: 'application/json',
-          dataType : 'json',
-          data: JSON.stringify({
-            "email": this.get('email'), 
-            "username": this.get('username'),
-            "password": this.get('password'),
-            "oldPassword": this.get('oldPassword')
-          })
-        }).then(newUser => {
-          // https://github.com/simplabs/ember-simple-auth/issues/926#issuecomment-243953558
-          var newSession = this.get('session.data');
-          newSession['authenticated']['token'] = newUser.user.token;
-          this.get('session.store').persist(newSession);
+      if (oldPassword) {
+        this.set('missingOldPassword', false);
+      } else {
+        this.set('missingOldPassword', true);
+      }
 
-          this.get('currentUser').update(newUser.user.email, newUser.user.username, newUser.user.token);
+      if (!this.get('missingOldPassword') && this.get('changedSetting')) {
+        new Ember.RSVP.Promise((/*resolve, reject*/) => {
+          Ember.$.ajax({
+            url: 'http://localhost:3000/api/users/',
+            type: 'PUT',
+            headers: { 'Authorization': `Bearer ${user.get('token')}` },
+            contentType: 'application/json',
+            dataType : 'json',
+            data: JSON.stringify({
+              "email": this.get('email'), 
+              "username": this.get('username'),
+              "password": this.get('password'),
+              "oldPassword": this.get('oldPassword')
+            })
+          }).then(newUser => {
+            // https://github.com/simplabs/ember-simple-auth/issues/926#issuecomment-243953558
+            var newSession = this.get('session.data');
+            newSession['authenticated']['token'] = newUser.user.token;
+            this.get('session.store').persist(newSession);
 
-          this.set('email', null);
-          this.set('username', null);
-          this.set('password', null);
-          this.set('oldPassword', null);
+            this.get('currentUser').update(newUser.user.email, newUser.user.username, newUser.user.token);
+
+            this.set('email', null);
+            this.set('username', null);
+            this.set('password', null);
+            this.set('oldPassword', null);
+          });
         });
-      });
+      }
     }
   }
 });
