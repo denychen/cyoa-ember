2b5cde97efeba51f2b478c1c7cb7fee742c37d0b
Deny Chen <deny.chen@gmail.com>
2017-08-01 18:17:07 -0700

    Added missing value checks for sign up page

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -2,22 +2,48 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
+  anyError: Ember.computed.or('missingEmail', 'missingPassword', 'missingUsername'),
+  noError: Ember.computed.not('anyError'),
   
   actions: {
     signup() {
-      this.get('store').createRecord('user', {
-        "email": this.get('email'),
-        "password": this.get('password'),
-        "username": this.get('username')
-      }).save().then(() => {
-        let credentials = { email: this.get('email'), password: this.get('password') };
+      let email = this.get('email');
+      let password = this.get('password');
+      let username = this.get('username');
 
-        this.set('email', null);
-        this.set('username', null);
-        this.set('password', null);
+      if (!email) {
+        this.set('missingEmail', true);
+      } else {
+        this.set('missingEmail', false);
+      }
 
-        return this.get('session').authenticate('authenticator:custom', credentials);
-      });
+      if (!password) {
+        this.set('missingPassword', true);
+      } else {
+        this.set('missingPassword', false);
+      }
+
+      if (!username) {
+        this.set('missingUsername', true);
+      } else {
+        this.set('missingUsername', false);
+      }
+
+      if (this.get('noError')) {
+        this.get('store').createRecord('user', {
+          email: email,
+          password: password,
+          username: username
+        }).save().then(() => {
+          let credentials = { email: this.get('email'), password: this.get('password') };
+
+          this.set('email', null);
+          this.set('username', null);
+          this.set('password', null);
+
+          return this.get('session').authenticate('authenticator:custom', credentials);
+        });
+      }
     }
   }
 });
