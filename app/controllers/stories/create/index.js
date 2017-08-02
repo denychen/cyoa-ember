59bffe2bb606c31818eddf16da36db9f4b3cd0ab
Deny Chen <deny.chen@gmail.com>
2017-08-01 17:49:11 -0700

    Update create story page to highlight input when erroring

--- app/controllers/stories/create/index.js
+++ app/controllers/stories/create/index.js
@@ -7,6 +7,8 @@ export default Ember.Controller.extend({
   maxTitleLength: 300,
   maxPremiseLength: 2000,
   genres: Ember.computed.readOnly('model'),
+  anyError: Ember.computed.or('missingTitle', 'missingGenre'),
+  noErrors: Ember.computed.not('anyError'),
 
   titleLength: Ember.computed('title', function() {
     return this.get('maxTitleLength') - (this.get('title.length') || 0);
@@ -18,20 +20,30 @@ export default Ember.Controller.extend({
 
   actions: {
     createStory() {
-      this.get('store').createRecord('story', {
-        title: this.get('title'),
-        published: false,
-        description: this.get('premise'),
-        authors: [this.get('currentUser.user')],
-        genres: this.get('selectedGenres').map(genre => {
-          return {
-            id: genre.id,
-            genre: genre.get('genre')
-          };
-        })
-      }).save().then(story => {
-        return this.transitionToRoute('stories.create.story', story.id);
-      });
+      let title = this.get('title');
+      let description = this.get('premise');
+      let author = this.get('currentUser.user');
+      let genres = this.get('selectedGenres');
+
+      title ? this.set('missingTitle', false) : this.set('missingTitle', true);
+      !Ember.isEmpty(genres) ? this.set('missingGenre', false) : this.set('missingGenre', true);
+      
+      if (this.get('noErrors')) {
+        this.get('store').createRecord('story', {
+          title: title,
+          published: false,
+          description: description,
+          authors: [author],
+          genres: genres.map(genre => {
+            return {
+              id: genre.id,
+              genre: genre.get('genre')
+            };
+          })
+        }).save().then(story => {
+          return this.transitionToRoute('stories.create.story', story.id);
+        });
+      }
     }
   }
 });
