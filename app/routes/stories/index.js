a794e7fbaccf3bbb4ad2ecfd9bdf9b340dddd59a
Deny Chen <deny.chen@gmail.com>
2017-08-10 20:14:11 -0700

    Clear store of stories when navigating to index stories page and my-stories page

--- app/routes/stories/index.js
+++ app/routes/stories/index.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel() {
+    return this.store.unloadAll('story');
+  },
+
   model() {
     return this.store.findAll('story');
   }
--- app/routes/stories/my-stories.js
+++ app/routes/stories/my-stories.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  beforeModel() {
+    return this.store.unloadAll('story');
+  },
+  
   model() {
     return this.store.query('story', { user: true });
   }
