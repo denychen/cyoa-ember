36d6942d182ba05f8bb08d29a72476f463336533
Deny Chen <deny.chen@gmail.com>
2017-08-10 18:36:28 -0700

    Allow fetching of next page of stories

--- app/controllers/stories/index.js
+++ app/controllers/stories/index.js
@@ -3,5 +3,27 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   stories: Ember.computed.readOnly('model'),
   storiesSorting: ['createdAt:desc'],
-  sortedStories: Ember.computed.sort('stories', 'storiesSorting')
+  sortedStories: Ember.computed.sort('stories', 'storiesSorting'),
+  paginatedStories: Ember.computed('sortedStories.[]', function() {
+    let storyLines = Ember.A();
+    let storyLine = Ember.A();
+    let numStories = this.get('sortedStories.length');
+
+    this.get('sortedStories').forEach((story, index) => {
+      storyLine.pushObject(story);
+
+      if (storyLine.length === 5 || index === numStories - 1) {
+        storyLines.pushObject(storyLine);
+        storyLine = Ember.A();
+      }
+    });
+
+    return storyLines;
+  }),
+
+  actions: {
+    loadMore(story) {
+      this.store.query('story', { id: story.get('id') });
+    }
+  }
 });
