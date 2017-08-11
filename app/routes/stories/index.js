c0e56b2deae134b876b7a9c65ba049bd0844a3d7
Deny Chen <deny.chen@gmail.com>
2017-08-10 20:54:22 -0700

    Do not reset index stories page stories when visiting the page, currently some bug preventing going into a story

--- app/routes/stories/index.js
+++ app/routes/stories/index.js
@@ -1,10 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  beforeModel() {
-    return this.store.unloadAll('story');
-  },
-
   model() {
     return this.store.findAll('story');
   }
