86324e9184960854a3711fe911154b8ecfab290a
Deny Chen <deny.chen@gmail.com>
2017-08-10 20:31:10 -0700

    Fix navigation in my-stories

--- app/routes/stories/my-stories.js
+++ app/routes/stories/my-stories.js
@@ -2,10 +2,6 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  beforeModel() {
-    return this.store.unloadAll('story');
-  },
-  
   model() {
     return this.store.query('story', { user: true });
   }
