5fda83e427331709cd91bb079c8b717a50647244
Deny Chen <deny.chen@gmail.com>
2017-08-15 14:53:18 -0700

    Add route transtion/unload confirmation for story creation page

--- app/routes/stories/create/index.js
+++ app/routes/stories/create/index.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import ConfirmationMixin from 'ember-onbeforeunload/mixins/confirmation';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+export default Ember.Route.extend(AuthenticatedRouteMixin, ConfirmationMixin, {
   model() {
     return this.get('store').createRecord('story');
   },
@@ -14,11 +15,34 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
     });
   },
 
+  shouldCheckIsPageDirty(transition) {
+    this._super(...arguments);
+    return false;
+  },
+
+  isPageDirty(model) {
+    if (model.get('dirtyType') === 'created') {
+      let dirtyTitle = !Ember.isEmpty(this.get('controller.title'));
+      let dirtyPremise = !Ember.isEmpty(this.get('controller.premise'));
+      let dirtyGenres = !Ember.isEmpty(this.get('controller.selectedGenres'));
+
+      return dirtyTitle || dirtyPremise || dirtyGenres;
+    } else {
+      return this._super(...arguments);
+    }
+  },
+
+  confirmationMessage: 'Changes you made may not be saved. Do you still want to continue?',
+
   actions: {
     willTransition() {
-      this.set('controller.title', null);
-      this.set('controller.premise', null);
-      this.set('controller.selectedGenres', Ember.A());
+      let transition = this._super(...arguments);
+      
+      if (transition) {
+        this.set('controller.title', null);
+        this.set('controller.premise', null);
+        this.set('controller.selectedGenres', Ember.A());
+      }
 
       return true;
     }
