01498b9156e1a5032d19103f2ab49f14010bdaac
Deny Chen <deny.chen@gmail.com>
2017-08-15 16:41:40 -0700

    Fix incorrect logic for page isDirty

--- app/components/page-item.js
+++ app/components/page-item.js
@@ -10,7 +10,7 @@ export default Ember.Component.extend({
   isTitleOrContentDirty: Ember.computed('page.id', 'page.name', 'page.content', 'page.hasDirtyAttributes', function() {
     if (this.get('page.id') && this.get('page.hasDirtyAttributes')) {
       let changedAttributes = this.get('page').changedAttributes();
-      return Object.values(changedAttributes).some(attributes => !attributes[0] !== !attributes[1]);
+      return Object.values(changedAttributes).some(attributes => attributes[0] !== attributes[1]);
     }
     
     return false;
