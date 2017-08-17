43fba5407828d3f9306ee6c4b84630cdef1069a8
Deny Chen <deny.chen@gmail.com>
2017-08-16 17:04:00 -0700

    Update deployment

---
+++ config/deploy.js
@@ -0,0 +1,44 @@
+/* jshint node: true */
+
+module.exports = function(deployTarget) {
+  var ENV = {
+    build: {},
+    // include other plugin configuration that applies to all deploy targets here
+    'revision-data': {
+      type: 'git-commit'
+    },
+    'ssh-index': {
+      remoteDir: "/usr/ocal/www/cyoa",
+      username: "pi",
+      host: "73.189.62.98",
+      privateKeyFile: "~/.ssh/id_rsa",
+      allowOverwrite: true
+    },
+    rsync: {
+      dest: "/usr/ocal/www/cyoa",
+      username: "pi",
+      host: "73.189.62.98",
+      delete: false
+    }
+  };
+
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+    // configure other plugins for development deploy target here
+  }
+
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+    // configure other plugins for staging deploy target here
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    // configure other plugins for production deploy target here
+  }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV;
+};
