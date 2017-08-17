4139dfa74b9ab8e2f54ad78493d2dc7cfe137792
Deny Chen <deny.chen@gmail.com>
2017-08-17 12:51:09 -0700

    Reset password forgot and reset forms on transition away

--- app/routes/forgot-password.js
+++ app/routes/forgot-password.js
@@ -2,4 +2,10 @@ import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
 export default Ember.Route.extend(UnauthenticatedRouteMixin, {
+  actions: {
+    willTransition() {
+      this.set('controller.missingEmail', false);
+      this.set('controller.emailSent', false);
+    }
+  }
 });
--- app/routes/reset-password.js
+++ app/routes/reset-password.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(UnauthenticatedRouteMixin, {
+  actions: {
+    willTransition() {
+      this.set('controller.missingResetToken', false);
+      this.set('controller.missingPassword', false);
+      this.set('controller.missingVerifyPassword', false);
+      this.set('controller.mismatchPasswords', false);
+      this.set('controller.errorMessage', null);
+    }
+  }
 });
