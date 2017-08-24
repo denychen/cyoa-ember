1f2381d6692e967e108ede35a81a85d5c23fbd0f
Deny Chen <deny.chen@gmail.com>
2017-08-24 16:22:55 -0700

    Fix date helper

--- app/helpers/date.js
+++ app/helpers/date.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export function date(params, hash) {
   let date = new Date(hash.date);
 
-  return `${date.getMonth() + 1}/${date.getDay()}/${date.getFullYear()}`
+  return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
 }
 
 export default Ember.Helper.helper(date);
