5081f2d5aa49aee6a61c44b1005bc4029a58e3ff
Deny Chen <deny.chen@gmail.com>
2017-08-24 12:11:38 -0700

    Simplify page dirtiness check by setting a default value of empty string for page name and premise

--- app/models/page.js
+++ app/models/page.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
   changeTracker: { trackHasMany: true, enableIsDirty: true, only: ['destinations'] },
 
   story: DS.belongsTo('story'),
-  content: DS.attr(),
-  name: DS.attr(),
+  content: DS.attr('string', { defaultValue: '' }),
+  name: DS.attr('string', { defaultValue: '' }),
   destinations: DS.hasMany('destination', { async: false })
 });
