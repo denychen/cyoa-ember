fe950c52874835aad48bf9aed338b13a8bef445f
Deny Chen <deny.chen@gmail.com>
2017-08-23 20:08:31 -0700

    Reset show more published/unpublished stories links on route load

--- app/routes/stories/my-stories.js
+++ app/routes/stories/my-stories.js
@@ -7,5 +7,12 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
       unpublished: this.store.query('story', { user: true, type: 'unpublished' }),
       published: this.store.query('story', { user: true, type: 'published' })
     });
+  },
+
+  setupController(controller, model) {
+    this._super(controller, model);
+    
+    this.set('controller.noMoreUnpublished', false);
+    this.set('controller.noMorepublished', false);
   }
 });
